742c5becefd875d9c18fbbfe742f0414e299d64a
denvudd <68691654+denvudd@users.noreply.github.com>
2021-04-06 23:00:27 +0300

    Секция интро и слайдер

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.categories-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: true,
+    centerMode: true,
+    initialSlide: 1,
+    autoplay: true,
+  });
+});
